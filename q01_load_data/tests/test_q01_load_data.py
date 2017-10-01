06ae438d893e4fb52eb265d6179a8cf651c04767
adimyth <mishraaditya6991@gmail.com>
2017-10-01 09:22:49 +0530

    Updates

--- q01_load_data/tests/test_q01_load_data.py
+++ q01_load_data/tests/test_q01_load_data.py
@@ -1,11 +1,9 @@
-import sys, os
 import pandas
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..', '..'))
 
 from unittest import TestCase
 from q01_load_data.build import load_data
 
 class TestLoad_data(TestCase):
     def test_load_data(self):
-        dataframe = load_data('../data/house_prices_multivariate.csv')
+        dataframe = load_data('data/house_prices_multivariate.csv')
         self.assertTrue(isinstance(dataframe, pandas.DataFrame))
--- q02_data_splitter/tests/test_q02_data_splitter.py
+++ q02_data_splitter/tests/test_q02_data_splitter.py
@@ -1,14 +1,19 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.curdir), '..', '..'))
+import pandas
 
 from unittest import TestCase
 from q02_data_splitter.build import data_splitter
 
-from q01_load_data.build import load_data
+# from q01_load_data.build import load_data
+
+def load_data(path):
+    dataframe = pandas.read_csv(path)
+    return dataframe
 
 class TestLoad_data(TestCase):
     def test_data_splitter(self):
-        dataframe = load_data('../data/house_prices_multivariate.csv')
+        dataframe = load_data('data/house_prices_multivariate.csv')
         X_house_prices, y_house_prices = data_splitter(dataframe)
         self.assertTrue('LotArea' in dataframe.columns.values)
         self.assertTrue('SalePrice' in dataframe.columns.values)

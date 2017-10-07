14311114a028d9bbab4e77262cdf05e95c53b460
Alex Anthony <alex19427@gmail.com>
2017-10-07 18:14:05 +0530

    Update build.py

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -2,11 +2,9 @@
 import pandas as pd
 import numpy as np
 
+path = 'data/housing_prices_multivariate.csv'
+
 # Your solution
 def load_data(path):
-
-    #Enter code here
-
-
-
+    # Enter code here
     return  dataframe

23f5479a16a76e730daa1b10da13caf7c95d327c
Alex Anthony <alex19427@gmail.com>
2017-10-07 20:46:07 +0530

    Update build.py

--- q05_residuals/build.py
+++ q05_residuals/build.py
@@ -1,10 +1,16 @@
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
+sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from q01_load_data.build import load_data
 from q02_data_splitter.build import data_splitter
 from q03_linear_regression.build import linear_regression
 from q04_linear_predictor.build import linear_predictor
+from sklearn.linear_model import LinearRegression
 
-def residuals(y, y_pred):
-    '''Enter Code Here'''
+dataframe = load_data('data/house_prices_multivariate.csv')
+X, y = data_splitter(dataframe)
+
+# call  linear_regression() and linear_predictor() function 
+
+def residuals(y=y, y_pred=y_pred):
+    # code here
     return error_residuals

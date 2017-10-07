f559ba4188b971408b6d6b5b5795b9aae5aa156d
Alex Anthony <alex19427@gmail.com>
2017-10-07 20:55:48 +0530

    Update build.py

--- q06_plot_residuals/build.py
+++ q06_plot_residuals/build.py
@@ -1,10 +1,20 @@
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
+sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from q01_load_data.build import load_data
 from q02_data_splitter.build import data_splitter
 from q03_linear_regression.build import linear_regression
 from q04_linear_predictor.build import linear_predictor
 from q05_residuals.build import residuals
+
+# Call matplotlib package and linear() function
+
+
+dataframe = load_data('data/house_prices_multivariate.csv')
+X, y = data_splitter(dataframe)
+linear_model = linear_regression(X,y)
+
+# call linear_predictor() and residuals() function
+
 
 def plot_residuals(y, error_residuals):
     '''Enter Code Here'''

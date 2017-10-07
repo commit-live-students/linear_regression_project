5e19d35844eb8aed84c90ff1cf1aa2a2f6dd476a
Alex Anthony <alex19427@gmail.com>
2017-10-07 19:59:57 +0530

    Update build.py

--- q03_linear_regression/build.py
+++ q03_linear_regression/build.py
@@ -3,6 +3,10 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
 from q01_load_data.build import load_data
 from q02_data_splitter.build import data_splitter
 
-def linear_regression(X, y):
-    '''Enter Code Here'''
+# Load the package for linear regression and use load_data() and data_splitter() function
+
+def linear_regression(X=X, y=y):
+    # Enter your code
+    
     return linear_regressor
+

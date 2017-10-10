b00697f8ccb81ff603cbc954113022899643c921
ABHISHEK THOMBRE <ug201314001@iitj.ac.in>
2017-10-10 13:21:21 +0530

    update in question3

--- q03_linear_regression/build.py
+++ q03_linear_regression/build.py
@@ -1,12 +1,14 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.curdir), '..'))
-from q01_load_data.build import load_data
-from q02_data_splitter.build import data_splitter
+from greyatomlib.linear_regression.q01_load_data.build import load_data
+from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
+from sklearn.linear_model import LinearRegression
 
 # Load the package for linear regression and use load_data() and data_splitter() function
+dataframe = load_data('data/house_prices_multivariate.csv')
+X, y = data_splitter(dataframe)
 
-def linear_regression(X=X, y=y):
+
+def linear_regression(X, y):
     # Enter your code
-    
+    linear_regressor = LinearRegression(normalize=False)
+    linear_regressor.fit(X, y)
     return linear_regressor
-
--- q03_linear_regression/tests/test_q03_linear_regression.py
+++ q03_linear_regression/tests/test_q03_linear_regression.py
@@ -1,13 +1,21 @@
 from unittest import TestCase
-from q03_linear_regression.build import linear_regression
+from ..build import linear_regression
+from greyatomlib.linear_regression.q01_load_data.build import load_data
+from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
+from inspect import getargspec
 
-from q01_load_data.build import load_data
-from q02_data_splitter.build import data_splitter
 
-class TestLoad_data(TestCase):
+class TestLinearRegression(TestCase):
     def test_linear_regression(self):
+        # Input parameters tests
+        args = getargspec(linear_regression)
+        self.assertEqual(len(args[0]), 2, "Expected argument(s) %d, Given %d" % (2, len(args[0])))
+        self.assertEqual(args[3], None, "Expected default values do not match given default values")
 
+        # Return type tests
         dataframe = load_data('data/house_prices_multivariate.csv')
         X_house_prices, y_house_prices = data_splitter(dataframe)
+
+        # Return value tests
         lm = linear_regression(X_house_prices, y_house_prices)
         self.assertTrue(lm.coef_.shape[0], 34)

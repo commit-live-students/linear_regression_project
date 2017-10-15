edb809e993e16ffaefc61af6d8974c976fe060ad
darshind <doshi.darshin@gmail.com>
2017-10-15 08:27:17 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,7 +1,13 @@
 # Default imports
 import pandas as pd
 
-path = 'data/house_prices_multivariate.csv'
+path = '/home/darshind/Workspace/code/linear_regression_project/data/house_prices_multivariate.csv'
 
 
 # Your code here
+def load_data(path):
+    df = pd.read_csv(path)
+    df.index.name = None
+    df.head()
+    return df
+print load_data(path)

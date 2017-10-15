1f94dd23e027fb20e5b057e8484171d2f41944c5
sudharsanasai <Sudharsanasai@gmail.com>
2017-10-15 13:32:28 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -4,4 +4,23 @@ import pandas as pd
 df = load_data('data/house_prices_multivariate.csv')
 
 
+def data_splitter(df):
+
+    df['Lot Frontage']=df.index
+    df.index = range(df.shape[0])
+    features = list(df.columns)
+    features = [features[-1]]+features[:-1]
+
+
+
+    #target = df['SalePrice']
+    #features = del list(df.columns)[len(list(df.columns))-1]
+    features.remove('SalePrice')
+    X = df[features]
+    y = df['SalePrice']
+
+
+    return X,y
+
+
 # Your Code Here

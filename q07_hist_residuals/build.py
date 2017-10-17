58539c622c4ebb57f629ed768cbcf63866c5e61c
apoorvabanubakode <apoorva.banubakode@gmail.com>
2017-10-17 19:42:15 +0000

    Done

--- q07_hist_residuals/build.py
+++ q07_hist_residuals/build.py
@@ -13,5 +13,8 @@ linear_model = linear_regression(X, y)
 y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
 error_residuals = residuals(y, y_pred)
 
+def hist_residuals(error_residuals,bins=60):
+    plt.hist(error_residuals,bins=bins)
+    plt.show()
 
 # Your code here

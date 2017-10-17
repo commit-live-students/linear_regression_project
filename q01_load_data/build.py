6ef264012776227b210f56bca9e78f8b2b05532b
samir0719 <sameer_0719@yahoo.com>
2017-10-17 16:36:52 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,8 +1,11 @@
-# %load q01_load_data/build.py
-# Default imports
+# %load q02_data_splitter/build.py
+# Default Imports
+from greyatomlib.linear_regression.q01_load_data.build import load_data
 import pandas as pd
-
-path = 'data/house_prices_multivariate.csv'
-def load_data(path):
-    df=pd.read_csv(path)
-    return df
+#df = load_data('data/house_prices_multivariate.csv')
+df=pd.read_csv('data/house_prices_multivariate.csv')
+def data_splitter(df):
+    y=df.iloc[:,-1]
+# Your Code Here
+    X=df.iloc[:,:-1]
+    return X,y
--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,7 +1,11 @@
+# %load q02_data_splitter/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 import pandas as pd
 df = load_data('data/house_prices_multivariate.csv')
-
-
+#df=pd.read_csv('data/house_prices_multivariate.csv')
+def data_splitter(df):
+    y=df.iloc[:,-1]
 # Your Code Here
+    X=df.iloc[:,:-1]
+    return X,y

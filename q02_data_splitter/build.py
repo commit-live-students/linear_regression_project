7491d6e07c3c5083678dd540e2d08543f9aa8e0b
shahprachi94 <prachirshah1994@gmail.com>
2017-10-18 18:18:13 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,7 +1,18 @@
+# %load q02_data_splitter/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 import pandas as pd
 df = load_data('data/house_prices_multivariate.csv')
+df.head()
+def data_splitter(df):
+#y = df.loc[:,'SalePrice']
+    X = df.iloc[:,:-1]
+    y = df.SalePrice
+
+
+
+    return X, y
+
 
 
 # Your Code Here

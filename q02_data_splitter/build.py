ac3867a819ac55e7798d72683d597721bb35daf6
arushikumar <arushikumar14@gmail.com>
2017-10-19 16:50:46 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -5,3 +5,18 @@ df = load_data('data/house_prices_multivariate.csv')
 
 
 # Your Code Here
+def data_splitter(df):
+    df=pd.read_csv('data/house_prices_multivariate.csv',index_col=None)
+    #print(df.shape)
+    X=df.iloc[:,:-1]
+    y=df.iloc[:,-1]
+
+    #print(X)
+    #print(type(y))
+
+    #print(X.shape)
+    #print(y.shape)
+
+    #print(y.iloc[4])
+    #print(X.iloc[4,3])
+    return X,y

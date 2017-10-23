f2d4c5d7adc3a2b7f5bade0a61b33fd3eeb23c61
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-23 17:09:18 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,16 +1,10 @@
-# Default Imports
+from greyatomlib.linear_regression.q01_load_data.build import load_data
 import pandas as pd
-import numpy as np
-df = pd.read_csv('data/house_prices_multivariate.csv', index_col=False)
+df = load_data('data/house_prices_multivariate.csv')
+
 def data_splitter(df) :
     #df = pd.read_csv('data/house_prices_multivariate.csv', index_col=False)
 
     # making Independent and Dependent variables from the dataset
     X = df.iloc[:,:-1]
     y = df.SalePrice
-    #df = pd.DataFrame(np.genfromtxt(path, dtype=dt))
-    #df = df.reset_index()
-
-
-
-# Your Code Here
--- q04_linear_predictor/build.py
+++ q04_linear_predictor/build.py
@@ -4,11 +4,23 @@ from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
 from greyatomlib.linear_regression.q03_linear_regression.build import linear_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
-
+import numpy as np
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
+def linear_predictor(linear_model,X, y):
+    y_pred = linear_model.predict(X)
+    mse = mean_squared_error(y_pred, y)
+    mae = mean_absolute_error(y_pred, y)
+    #rse = r2_score(y_pred, y)
+    SS_Residual = sum((y-y_pred)**2)
+    SS_Total = sum((y-np.mean(y))**2)
+    r_squared = 1 - (float(SS_Residual))/SS_Total
 
+    return y_pred,mse,mae, r_squared
+
+
+
+#linear_predictor(linear_model,X, y)
 
 # Your code here

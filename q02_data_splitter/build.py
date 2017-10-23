f59a015d2e22bc072f620daae3ed662bbb77bd5d
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-23 16:43:06 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,16 +1,16 @@
 # Default Imports
 import pandas as pd
 import numpy as np
-def data_splitter() :
+df = pd.read_csv('data/house_prices_multivariate.csv', index_col=False)
+def data_splitter(df) :
+    #df = pd.read_csv('data/house_prices_multivariate.csv', index_col=False)
 
-    df = pd.read_csv('data/house_prices_multivariate.csv', index_col=False)
     # making Independent and Dependent variables from the dataset
     X = df.iloc[:,:-1]
     y = df.SalePrice
     #df = pd.DataFrame(np.genfromtxt(path, dtype=dt))
     #df = df.reset_index()
 
-    return (X, y)
 
 
 # Your Code Here

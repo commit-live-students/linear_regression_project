40a915e46a27359238fc6b4772f191f3e9eab4ac
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-12-25 03:22:37 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -2,6 +2,11 @@
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 import pandas as pd
 df = load_data('data/house_prices_multivariate.csv')
+from sklearn.model_selection import train_test_split
 
 
 # Your Code Here
+def data_splitter(data):
+    y = df.pop('SalePrice')
+    X = df
+    return X, y

ca489bd31e9053e4da9ca368d333b11b58454db5
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-12-26 15:34:32 +0000

    Done

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
+from greyatomlib.linear_regression.q01_load_data.build import load_data
+from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
+from greyatomlib.linear_regression.q03_linear_regression.build import linear_regression
+from greyatomlib.linear_regression.q04_linear_predictor.build import linear_predictor
+from greyatomlib.linear_regression.q05_residuals.build import residuals
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import pylab
 
+dataframe = load_data('data/house_prices_multivariate.csv')
+X, y = data_splitter(dataframe)
+linear_model = linear_regression(X, y)
+y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
+error_residuals = residuals(y, y_pred)
+
+def qq_residuals(error_residuals):
+    stats.probplot(error_residuals, dist='norm', plot=pylab)
+    pylab.show()
+    return None

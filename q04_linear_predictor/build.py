e156fbec8eae3c3bb25924f0c3f9a3f7b88930e6
nazirwa <nazirmail@gmail.com>
2018-01-01 10:39:06 +0000

    Done

--- q04_linear_predictor/build.py
+++ q04_linear_predictor/build.py
@@ -12,3 +12,37 @@ linear_model = linear_regression(X, y)
 
 
 # Your code here
+def linear_predictor(lm, X, y):
+    # Using the linear regression object lm, prdict y value
+    y_pred = lm.predict(X)
+
+    # We need to use the predicted and actual values of y
+    # to calculate MAE and MSE values
+    # Adding the predicted value into the original data frame
+    # Here SalePrice is the actual value
+    dataframe["SalePricePredicted"] = lm.predict(X)
+
+    # We can now use these two columns to calculate the errors
+    mse = mean_squared_error(dataframe["SalePrice"], dataframe["SalePricePredicted"])
+
+    mae = mean_absolute_error(dataframe["SalePrice"], dataframe["SalePricePredicted"])
+
+    r2 = r2_score(dataframe["SalePrice"], dataframe["SalePricePredicted"])
+
+    # Voila
+    return y_pred, mse, mae, r2
+
+
+
+# y, mse, mae, r2 = linear_predictor(linear_model, X, y)
+# print {"##### Testing #####"}
+# print "Predicted Y value type: {0}".format(type(y))
+# print "Predicted Y value: {0}".format(y)
+#
+# # Print out the errors for testing
+# print "Mean Squared Error: {0}".format(mse)
+# print "MSE Type: {0}".format(type(mse))
+# print "Mean Absolute Error: {0}".format(mae)
+# print "MAE Type: {0}".format(type(mae))
+# print "R Squared Score : {0}".format(r2)
+# print "R2 Type: {0}".format(type(r2))

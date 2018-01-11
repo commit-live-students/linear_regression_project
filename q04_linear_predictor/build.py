6f1c17db838b13d75e72f66929f079702264abb8
dharmil25 <dharmil2524_shah@ymail.com>
2018-01-11 11:50:16 +0000

    Done

--- q04_linear_predictor/build.py
+++ q04_linear_predictor/build.py
@@ -12,3 +12,9 @@ linear_model = linear_regression(X, y)
 
 
 # Your code here
+def linear_predictor(lm, X, y):
+    y_pred = linear_model.predict(X)
+    mse = mean_squared_error(y_pred, y)
+    mae = mean_absolute_error(y_pred, y)
+    r2 = r2_score(y, y_pred)
+    return y_pred, mse, mae, r2

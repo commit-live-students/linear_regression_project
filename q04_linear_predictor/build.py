86f40e3e7b607e25c1827418e9c086ea96f32e0a
vp999 <patilvish@gmail.com>
2018-01-13 10:31:25 +0000

    Done

--- q04_linear_predictor/build.py
+++ q04_linear_predictor/build.py
@@ -12,3 +12,20 @@ linear_model = linear_regression(X, y)
 
 
 # Your code here
+import numpy as np
+
+def linear_predictor(lm,X,y):
+    y_pred = lm.predict(X)
+    n= len(y)
+    sse = np.float64( ((y- y_pred)**2).sum() )
+    mse = np.float64( sse/n)
+    mae = np.float64( (abs(y- y_pred)).sum()/n  )
+    sst = ((y-y.mean())**2).sum()
+    r2= np.float64(  1 - (sse/sst))
+    return y_pred ,mse,mae, r2
+
+y_pred,mse,mae,r2 = linear_predictor(linear_model,X,y)
+
+#print 'mae : ' , mae
+#print 'mse : ' , mse
+#print 'r2 : ',r2

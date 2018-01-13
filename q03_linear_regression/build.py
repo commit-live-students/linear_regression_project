c3915fefa5d0d9cd40246ba75ad821bcfa1dd717
vp999 <patilvish@gmail.com>
2018-01-13 09:57:13 +0000

    Done

--- q03_linear_regression/build.py
+++ q03_linear_regression/build.py
@@ -4,3 +4,9 @@ from sklearn.linear_model import LinearRegression
 
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
+
+
+def linear_regression(X,y):
+    return LinearRegression().fit( X,y )
+
+print linear_regression(X,y)

8a4dc68ac0bcfdd34a4deed7b4b0c7b0e74188ad
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-01-27 14:53:54 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,7 +1,15 @@
+# %load q02_data_splitter/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 import pandas as pd
+from sklearn.svm import LinearSVC
 df = load_data('data/house_prices_multivariate.csv')
 
+def data_splitter(df):
+    y = df['SalePrice']
+    del df['SalePrice']
+    X = df
+    return X,y
 
-# Your Code Here
+print data_splitter(df)
+

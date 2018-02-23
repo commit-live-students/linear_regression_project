fe8bf47d8b415ff6c72e190cecc093c9b749a459
viishnu712 <viishnu712@gmail.com>
2018-02-23 15:31:53 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -4,4 +4,14 @@ import pandas as pd
 df = load_data('data/house_prices_multivariate.csv')
 
 
-# Your Code Here
+def data_splitter(data):
+    X = data[['LotFrontage', u'LotArea', u'OverallQual', u'OverallCond',
+       u'YearBuilt', u'YearRemodAdd', u'MasVnrArea', u'BsmtFinSF1',
+       u'BsmtFinSF2', u'BsmtUnfSF', u'TotalBsmtSF', u'1stFlrSF', u'2ndFlrSF',
+       u'LowQualFinSF', u'GrLivArea', u'BsmtFullBath', u'BsmtHalfBath',
+       u'FullBath', u'HalfBath', u'BedroomAbvGr', u'KitchenAbvGr',
+       u'TotRmsAbvGrd', u'Fireplaces', u'GarageYrBlt', u'GarageCars',
+       u'GarageArea', u'WoodDeckSF', u'OpenPorchSF', u'EnclosedPorch',
+       u'3SsnPorch', u'ScreenPorch', u'PoolArea', u'MiscVal', u'YrSold']]
+    y= data['SalePrice']
+    return (X),(y)

cce5266768ae668bb2db5234aba818464f2325fd
Srishtisawla <sawlasrishti@gmail.com>
2018-02-27 13:10:32 +0000

    Done

--- q07_hist_residuals/build.py
+++ q07_hist_residuals/build.py
@@ -12,6 +12,6 @@ X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
 y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
 error_residuals = residuals(y, y_pred)
-
-
-# Your code here
+def hist_residuals(error_residuals,bin=60):
+    plt.hist(error_residuals,bins=bin)
+    plt.show()

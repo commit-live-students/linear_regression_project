937039ea287ad75b6b109f2d6a37dff434592000
Srishtisawla <sawlasrishti@gmail.com>
2018-02-27 12:52:50 +0000

    Done

--- q04_linear_predictor/build.py
+++ q04_linear_predictor/build.py
@@ -9,6 +9,11 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
-
+def linear_predictor(linear_model,X,y):
+    y_predict = linear_model.predict(X)
+    r2 = r2_score(y,y_predict)
+    mse = mean_squared_error(y,y_predict)
+    mae = mean_absolute_error(y,y_predict)
+    return y_predict,mse,mae,r2
 
 # Your code here

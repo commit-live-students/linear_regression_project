69746e9612ef7f3f108a8dada1d01ab33f6dedc5
PBPatil <theprasadpatil@gmail.com>
2018-03-03 22:47:57 +0000

    Done

--- q07_hist_residuals/build.py
+++ q07_hist_residuals/build.py
@@ -10,8 +10,11 @@ import matplotlib.pyplot as plt
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
-y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
+y_pred,mse, mae,r2 = linear_predictor(linear_model, X, y)
 error_residuals = residuals(y, y_pred)
 
 
 # Your code here
+def hist_residuals(error_residuals,bins=60):
+    plt.hist(error_residuals)
+    plt.show()

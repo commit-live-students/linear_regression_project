88e37bc47955d7d3af889e7a3e3dc7a426f3556d
HerambD <heramb.dharmadhikari@gmail.com>
2018-03-04 17:41:25 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -6,6 +6,8 @@ df = load_data('data/house_prices_multivariate.csv')
 
 # Your Code Here
 def data_splitter(df):
+    #X is df contaning Dependent Variables
     X = df.iloc[:,:-1]
+    #y is Target Variables
     y = df.SalePrice
     return X, y

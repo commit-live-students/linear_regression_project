ed126d1570a72b598c1bffd21afa16d60a64ca82
TheSarang <narkhedesarang@gmail.com>
2018-03-04 08:33:47 +0000

    Done

--- q05_residuals/build.py
+++ q05_residuals/build.py
@@ -1,3 +1,4 @@
+# %load q05_residuals/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
@@ -10,5 +11,6 @@ X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
 y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
 
+def residuals(y, y_pred):
+    return (y - y_pred)
 
-# Your code here

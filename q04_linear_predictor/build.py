c179e3fca8f4f4a2f64694d4a2106b7870bacbf3
aditya81295 <patiladitya81295@gmail.com>
2018-03-04 15:38:45 +0000

    Done

--- q04_linear_predictor/build.py
+++ q04_linear_predictor/build.py
@@ -10,5 +10,19 @@ dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
 
+y_len=len(y)
 
 # Your code here
+
+def linear_predictor(lm,X,y):
+    lr= linear_regression()
+    y_pred=lr.predict(X)
+    mse=mean_squared_error(y_pred, y)
+    mae=mean_absolute_error(y_pred,y)
+    sst=0
+    ssr=0
+    for i in range(y_len):
+        sst = sst+(y[i] - y.mean())**2
+        ssr = ssr+(y_pred[i] - y.mean())**2
+    r2=ssr/sst
+    return y_pred,mse,mae,r2

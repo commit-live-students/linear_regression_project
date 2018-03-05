d2d0ea1babb62dc60a946d608a047744f5436206
seema200 <singh.seema200@gmail.com>
2018-03-05 11:38:59 +0000

    Done

--- q06_plot_residuals/build.py
+++ q06_plot_residuals/build.py
@@ -13,5 +13,11 @@ linear_model = linear_regression(X, y)
 y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
 error_residuals = residuals(y, y_pred)
 
-
 # Your code here
+
+def plot_residuals(y,error_residuals):
+    plt.scatter(y,error_residuals)
+    plt.xlabel('SalePrice')
+    plt.ylabel('Errors')
+    plt.title('Residuals Plot')
+    plt.show()

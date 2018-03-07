7cbedf87a21402e40c1c3e0f38dd205cd2c339af
inivikrant <inivikrant@gmail.com>
2018-03-07 13:18:47 +0000

    Done

--- q03_linear_regression/build.py
+++ q03_linear_regression/build.py
@@ -6,6 +6,6 @@ dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 def linear_regression(X, y):
     lr = LinearRegression()
-    intercept = lr.intercept_
-    return(lr, intercept)
-print(linear_regression(X, y))
+    lr1 = lr.fit(X, y)
+    return(lr1)
+print((linear_regression(X,y)))

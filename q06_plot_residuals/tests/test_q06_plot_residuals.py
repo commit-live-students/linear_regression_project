12a97fb4139e1fe0930db9af6fcd44cc49536e1d
Abhishek Subramanian <abhisheksubu92@gmail.com>
2018-03-09 12:05:31 +0530

    Update test_q06_plot_residuals.py

--- q06_plot_residuals/tests/test_q06_plot_residuals.py
+++ q06_plot_residuals/tests/test_q06_plot_residuals.py
@@ -1,17 +1,16 @@
-from unittest import TestCase
 from ..build import plot_residuals
 from unittest import TestCase
-from inspect import getargspec
-
+from inspect import getfullargspec
+from unittest import TestCase
 
 class TestPlotResiduals(TestCase):
     def test_plot_residuals_arguements(self):
 
         # Input parameters tests
-        args = getargspec(plot_residuals)
+        args = getfullargspec(plot_residuals)
         self.assertEqual(len(args[0]), 2, "Expected argument(s) %d, Given %d" % (2, len(args[0])))
     def test_plot_residuals_defaults(self):
-    	args = getargspec(plot_residuals)
+        args = getfullargspec(plot_residuals)
         self.assertEqual(args[3], (None), "Expected default values do not match given default values")
 
         # Return type tests

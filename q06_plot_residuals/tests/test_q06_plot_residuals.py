d28b38c060a16bd6d3fe1eecf981df48be500dc1
Alex Anthony <alex19427@gmail.com>
2018-03-12 13:31:51 +0530

    Update test_q06_plot_residuals.py

--- q06_plot_residuals/tests/test_q06_plot_residuals.py
+++ q06_plot_residuals/tests/test_q06_plot_residuals.py
@@ -1,13 +1,13 @@
 from ..build import plot_residuals
 from unittest import TestCase
-from inspect import getfullargspec
+from inspect import getargspec
 from unittest import TestCase
 
 class TestPlotResiduals(TestCase):
     def test_plot_residuals_arguements(self):
 
         # Input parameters tests
-        args = getfullargspec(plot_residuals)
+        args = getargspec(plot_residuals)
         self.assertEqual(len(args[0]), 2, "Expected argument(s) %d, Given %d" % (2, len(args[0])))
    
 

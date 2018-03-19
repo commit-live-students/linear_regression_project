6f9eb8e02c0fea3988db5ac75dec94e0b576e814
Ketan10994 <katcee10@gmail.com>
2018-03-19 08:18:40 +0000

    Done

--- q04_linear_predictor/build.py
+++ q04_linear_predictor/build.py
@@ -10,5 +10,31 @@ dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
 
+# %load q04_linear_predictor/build.py
+# Default Imports
+from greyatomlib.linear_regression.q01_load_data.build import load_data
+from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
+from greyatomlib.linear_regression.q03_linear_regression.build import linear_regression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+
+dataframe = load_data('data/house_prices_multivariate.csv')
+X, y = data_splitter(dataframe)
+linear_model = linear_regression(X, y)
+
 
 # Your code here
+def linear_predictor(lm,X,y):
+    #lm.fit(X,y)
+    y_predict = lm.predict(X)
+    mse = mean_squared_error(y_predict,y)
+    mae = mean_absolute_error(y_predict , y)
+    r2 = r2_score(y,y_predict)
+    return (y_predict , mse , mae , r2)
+
+linear_predictor(linear_model,X,y)
+
+#linear_model.fit(X,y)
+#y_pred = linear_model.predict(X)
+#mean_squared_error(y_pred , y)

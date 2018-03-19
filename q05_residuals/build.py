15ef7436738effa305522c75d45fc077306d9504
Ketan10994 <katcee10@gmail.com>
2018-03-19 08:23:31 +0000

    Done

--- q05_residuals/build.py
+++ q05_residuals/build.py
@@ -10,5 +10,9 @@ X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
 y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
 
+def residuals(y , y_pred):
+    r = y - y_pred
+    return r
 
+residuals(y , y_pred)
 # Your code here

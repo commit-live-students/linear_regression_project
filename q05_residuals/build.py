df3656e7adf2900413639e68f6323ff138a9976a
ga-ashlin-dev <ashlin+78@greyatom.com>
2018-04-03 12:30:13 +0000

    Done

--- q05_residuals/build.py
+++ q05_residuals/build.py
@@ -1,4 +1,5 @@
 
+# Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
 from greyatomlib.linear_regression.q03_linear_regression.build import linear_regression
@@ -9,8 +10,4 @@ dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
 y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
-
-def residuals(y, y_pred):
-    error_residuals = y - y_pred
-    return error_residuals
 

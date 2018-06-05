3521182dfb46a09627e3c4eb5dc6262b00517f93
sanchitaharlalka <Sanchitaharlalka@gmail.com>
2018-06-05 18:31:01 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,3 +1,4 @@
+# %load q01_load_data/build.py
 # Default imports
 import pandas as pd
 
@@ -5,4 +6,12 @@ path = 'data/house_prices_multivariate.csv'
 
 
 # Write your code here :
+def load_data(path):
+    df=pd.read_csv('data/house_prices_multivariate.csv',index_col=None)
+    df.reset_index(drop=True,inplace=True)
+    return df
+
+#df=load_data(path)
+#df
+
 

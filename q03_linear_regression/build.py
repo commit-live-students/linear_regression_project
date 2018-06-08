c30120cea9f3870f3bf25379f984257b6c3f5352
swapcy <email2swap@gmail.com>
2018-06-08 11:09:40 +0000

    Done

--- q03_linear_regression/build.py
+++ q03_linear_regression/build.py
@@ -1,10 +1,15 @@
+# %load q03_linear_regression/build.py
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
 from sklearn.linear_model import LinearRegression
 
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
-
 # Write your code here :
+def linear_regression(X,y):
+    regression = LinearRegression()
+    lm = regression.fit(X,y)
+    return lm
 
+#type(linear_regression(X,y))
 

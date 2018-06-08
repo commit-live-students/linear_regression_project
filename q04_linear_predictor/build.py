658f8e384d35e6fd9dfd3e37508948fe60971d8e
kishangooner <kishansk26@gmail.com>
2018-06-08 10:37:43 +0000

    Done

--- q04_linear_predictor/build.py
+++ q04_linear_predictor/build.py
@@ -1,3 +1,4 @@
+# %load q04_linear_predictor/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
@@ -10,5 +11,11 @@ dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
 
+def linear_predictor(lm,X,y):
+    y_pred=lm.predict(X)
+    return(y_pred,mean_squared_error(y,y_pred),mean_absolute_error(y,y_pred),r2_score(y,y_pred))
+    
 
 # Your code here
+
+

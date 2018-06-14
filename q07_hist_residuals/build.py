8d473e0c86a60f0d79349e87ec127d00d741f58d
vnylp <vinitdhamale.we@gmail.com>
2018-06-14 14:12:53 +0000

    Done

--- q07_hist_residuals/build.py
+++ q07_hist_residuals/build.py
@@ -1,3 +1,4 @@
+# %load q07_hist_residuals/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
@@ -5,12 +6,13 @@ from greyatomlib.linear_regression.q03_linear_regression.build import linear_reg
 from greyatomlib.linear_regression.q04_linear_predictor.build import linear_predictor
 from greyatomlib.linear_regression.q05_residuals.build import residuals
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 
 
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
-y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
+y_pred,mse, mae,r2 = linear_predictor(linear_model, X, y)
 error_residuals = residuals(y, y_pred)
 
 
@@ -18,3 +20,7 @@ error_residuals = residuals(y, y_pred)
 def hist_residuals(error_residuals, bins=60):
     plt.figure(figsize=(15,8))
     plt.hist(error_residuals, bins=bins)
+    return plt.show()
+    
+hist_residuals(error_residuals, bins=60)
+

c0e9403df06e6756f63bbae0f4e5d1cbb155f25a
naitik1995 <naitikgada1995@gmail.com>
2018-06-15 17:48:54 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -1,3 +1,4 @@
+# %load q02_data_splitter/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 import pandas as pd
@@ -5,4 +6,13 @@ df = load_data('data/house_prices_multivariate.csv')
 
 
 # Your Code Here
+def data_splitter(df):
+    #feature variable having all values except SalePrice
+   
+    #Target variable only sale Price
+    y = df.iloc[:,-1]
+    X = df.loc[:,df.columns != 'SalePrice']
+    return X,y
+data_splitter(df)
+
 

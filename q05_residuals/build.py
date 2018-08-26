d6e50073b2ef67505127d4627705452b4ad09d47
Tusharsharma86 <tusharsharma86@gmail.com>
2018-08-26 16:56:07 +0000

    Done

--- q05_residuals/build.py
+++ q05_residuals/build.py
@@ -1,3 +1,4 @@
+# %load q05_residuals/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
@@ -8,7 +9,15 @@ from sklearn.linear_model import LinearRegression
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
-y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
+y_pred, mse, mae, r2 = linear_predictor(linear_model, X, y)
 
 
-# Your code here
+# Funtions to calculate residuals
+def residuals(y,y_pred):
+    error_residuals = y - y_pred
+    return error_residuals
+
+residuals(y,y_pred)
+
+
+

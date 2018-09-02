3b15c1b40f4202e3ccdbf528a0e3469df79e6df8
madhav2712 <mishra.madhav4@gmail.com>
2018-09-02 06:13:27 +0000

    Done

--- q06_plot_residuals/build.py
+++ q06_plot_residuals/build.py
@@ -16,4 +16,9 @@ error_residuals = residuals(y, y_pred)
 
 
 # Your code here
-
+def plot_residuals(y,error_residuals):
+    plt.scatter(y,error_residuals)
+    plt.xlabel('SalePrice')
+    plt.ylabel('Errors')
+    plt.title('Residuals Plot')
+    plt.show()

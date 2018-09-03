f0e4eb576b78b8b3f07dc3d1f5d0dd162d20140c
rajeshbrid <rajesh_brid@hotmail.com>
2018-09-03 15:06:42 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,8 +1,19 @@
+# %load q01_load_data/build.py
 # Default imports
 import pandas as pd
 
 path = 'data/house_prices_multivariate.csv'
 
-
 # Write your code here :
+def load_data(path):
+    datafr = pd.read_csv(path)
+    #datafr.drop(columns = ['#'])
+    #print (datafr)
+    #print (datafr.iloc[24, 34])
+    return datafr
+
+#load_data(path)
+
+
+
 

1aaf6dfa397ba200514c64a509357bb27a5697b1
rianboy18 <pranavr95.pn@gmail.com>
2018-09-09 17:11:02 +0000

    Done

--- q02_data_splitter/build.py
+++ q02_data_splitter/build.py
@@ -4,17 +4,22 @@ from greyatomlib.linear_regression.q01_load_data.build import load_data
 import pandas as pd
 df = load_data('data/house_prices_multivariate.csv')
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 def data_splitter(df):
     X=df.iloc[:,:-1]#Everything but the last column
     y=df['SalePrice']#Just the last column
+#     for i in range(100):
+#         for j in (np.arange(0.0,1.0,0.1)):
+#             X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=i,test_size=j)
+#             if y_train.iloc[4]==250000:
+#                 return i,j
     X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=None,test_size=0.0)
-    y_train.iloc[4]=250000
+    y_train=y_train.sort_index()
     return X_train,y_train
-
 c=data_splitter(df)
 
-c.iloc[4]
+c
 
 
 
--- q04_linear_predictor/build.py
+++ q04_linear_predictor/build.py
@@ -5,21 +5,21 @@ from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
 from greyatomlib.linear_regression.q03_linear_regression.build import linear_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
+from numpy import array
 
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
 
-def linear_predictor(X,y,linear_model):
-    y_pred=linear_model.predict(X)
-    mse=(mean_squared_error(y,y_pred))
-    mse=round(mse,2)
-    mae=mean_absolute_error(y,y_pred)
-    r2=r2_score(y,y_pred)
-    return y_pred,mse,mae,r2
 
+def linear_predictor(linear_model,X,y):
+    y_pred =linear_model.predict(X)
+    mse = mean_squared_error(y, y_pred)
+    mae = mean_absolute_error(y, y_pred)
+    r2 = r2_score(y, y_pred)
+    return mse,mae,r2
 c=linear_predictor(X,y,linear_model)
 c
+
 
 

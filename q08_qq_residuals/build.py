0db3a94744f5a5a15e41ae85bf06e8f68d88dd49
ankit-a-mehta <ankitmehta195@gmail.com>
2018-10-14 08:49:12 +0000

    Done

--- q08_qq_residuals/build.py
+++ q08_qq_residuals/build.py
@@ -1,3 +1,4 @@
+# %load q08_qq_residuals/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
@@ -20,3 +21,11 @@ error_residuals = residuals(y, y_pred)
 
 
 # Your code here
+import numpy as np
+def qq_residuals(error_residuals):
+    measurements = np.random.normal(loc = 100, scale = 10000, size=X.shape[0])   
+    stats.probplot(measurements, dist='norm', plot=pylab)
+    pylab.show()
+
+qq_residuals(error_residuals)
+

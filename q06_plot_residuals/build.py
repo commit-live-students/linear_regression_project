05af4f423890fa83793b69d0763d8d54d3164332
KamalShillongwala <qamaldas@gmail.com>
2018-10-21 07:26:26 +0000

    Done

--- q06_plot_residuals/build.py
+++ q06_plot_residuals/build.py
@@ -1,3 +1,4 @@
+# %load q06_plot_residuals/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
@@ -11,9 +12,15 @@ plt.switch_backend('agg')
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
-y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
+y_pred, mse, mae, r2 = linear_predictor(linear_model, X, y)
 error_residuals = residuals(y, y_pred)
 
 
 # Your code here
+def plot_residuals(y, error_residuals):
+    plt.scatter(y,error_residuals)
+    plt.show()
+
+
+
 

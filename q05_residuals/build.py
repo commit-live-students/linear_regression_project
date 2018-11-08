34ed11f513596ab5f99c4efd35edc00caf4bdef5
preetiail <ailpreeti@ymail.com>
2018-11-08 08:47:45 +0000

    Done

--- q05_residuals/build.py
+++ q05_residuals/build.py
@@ -1,14 +1,21 @@
+# %load q05_residuals/build.py
 # Default Imports
 from greyatomlib.linear_regression.q01_load_data.build import load_data
 from greyatomlib.linear_regression.q02_data_splitter.build import data_splitter
 from greyatomlib.linear_regression.q03_linear_regression.build import linear_regression
 from greyatomlib.linear_regression.q04_linear_predictor.build import linear_predictor
 from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score
 
 dataframe = load_data('data/house_prices_multivariate.csv')
 X, y = data_splitter(dataframe)
 linear_model = linear_regression(X, y)
 y_pred, _, __, ___ = linear_predictor(linear_model, X, y)
 
+def residuals(y,y_pred):
+    RSE=pd.Series([y[i]-y_pred[i] for i in range(len(dataframe))])
+    return(RSE)
+residuals(y,y_pred)
 
-# Your code here

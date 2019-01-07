f8affe51ade40e038b2666cb90812cef2523e6e1
sskewal <sskewal@yahoo.in>
2019-01-07 22:08:52 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,8 +1,14 @@
+# %load q01_load_data/build.py
 # Default imports
 import pandas as pd
 
 path = 'data/house_prices_multivariate.csv'
 
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+load_data(path)
 
-# Write your code here :
+
+
 
